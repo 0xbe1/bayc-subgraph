35c2c396203d48c578ba0049b764f89a8cb09542
0xbe1 <0xbetrue@gmail.com>
2022-03-18 18:20:08 +0800

    rewrite in old style

--- src/mapping.ts
+++ src/mapping.ts
@@ -11,39 +11,41 @@ const ipfshash = "QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq"
 
 export function handleTransfer(event: TransferEvent): void {
   /* load the token from the existing Graph Node */
-  const tryToken = Token.load(event.params.tokenId.toString())
-  const token = tryToken ? tryToken : new Token(event.params.tokenId.toString())
-    
-  token.tokenID = event.params.tokenId
-  token.tokenURI = "/" + event.params.tokenId.toString()
-
-  /* combine the ipfs hash and the token ID to fetch the token metadata from IPFS */
-  let metadata = ipfs.cat(ipfshash + token.tokenURI)
-  if (metadata) {
-    const value = json.fromBytes(metadata).toObject()
-    if (value) {
-      /* using the metatadata from IPFS, update the token object with the values  */
-      const image = value.get('image')
-      if (image) {
-        token.image = image.toString()
-        token.ipfsURI = 'ipfs.io/ipfs/' + ipfshash + token.tokenURI
-      }
-
-      const attributes = value.get('attributes')
-      if (attributes) {
-        const arr = attributes.toArray()
-        for (let i = 0; i < arr.length; i++) {
-          const attr = arr[i]  
-          const obj = attr.toObject()
-          const traitType = obj.mustGet('trait_type').toString()
-          const value = obj.mustGet('value').toString()
-          if (traitType === "Background") token.background = value
-          if (traitType === "Clothes") token.clothes = value
-          if (traitType === "Earring") token.earring = value
-          if (traitType === "Eyes") token.eyes = value
-          if (traitType === "Fur") token.fur = value
-          if (traitType === "Hat") token.hat = value
-          if (traitType === "Mouth") token.mouth = value
+  let token = Token.load(event.params.tokenId.toString())
+  if (!token) {
+    token = new Token(event.params.tokenId.toString())
+    token.tokenID = event.params.tokenId
+    token.tokenURI = "/" + event.params.tokenId.toString()
+  
+    /* combine the ipfs hash and the token ID to fetch the token metadata from IPFS */
+    let metadata = ipfs.cat(ipfshash + token.tokenURI)
+    if (metadata) {
+      const value = json.fromBytes(metadata).toObject()
+      if (value) {
+        /* using the metatadata from IPFS, update the token object with the values  */
+        const image = value.get('image')
+        if (image) {
+          token.image = image.toString()
+          token.ipfsURI = 'ipfs.io/ipfs/' + ipfshash + token.tokenURI
+        }
+  
+        const attributes = value.get('attributes')
+        if (attributes) {
+          const arr = attributes.toArray()
+          for (let i = 0; i < arr.length; i++) {  
+            const obj = arr[i].toObject()
+            const traitType = obj.get('trait_type')
+            const value = obj.get("value")
+            if (traitType && value) {
+              if (traitType.toString() === "Background") token.background = value.toString()
+              if (traitType.toString() === "Clothes") token.clothes = value.toString()
+              if (traitType.toString() === "Earring") token.earring = value.toString()
+              if (traitType.toString() === "Eyes") token.eyes = value.toString()
+              if (traitType.toString() === "Fur") token.fur = value.toString()
+              if (traitType.toString() === "Hat") token.hat = value.toString()
+              if (traitType.toString() === "Mouth") token.mouth = value.toString()
+            }
+          }
         }
       }
     }
